3aea45fab5bc5f2b004d8b6f6f973a22336630dd
li xie <xie00053@algonquinlive.com>
2019-08-07 10:46:52 -0400

    SignIn,SignUp,localstorage,error working

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,73 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'home' }">Home</router-link>
+      <router-link v-if="!auth" :to="{ name: 'signup' }">Sign Up</router-link>
+      <router-link v-if="!auth" :to="{ name: 'signin' }">Sign In</router-link>
+      <router-link v-if="auth" :to="{ name: 'dashboard' }">Dashboard</router-link>
+      <a v-if="auth" class="logout" @click="logout"> Log Out</a>
     </div>
+
+<!-- if the error is set, diaplay the div, the info of the div is the {{error}} -->
+    <div class="error" @click="clearError" v-if="error">{{error}}</div>
+
     <router-view />
+    
   </div>
 </template>
 
+<script>
+import { mapState, mapActions, mapGetters} from "vuex";
+// import { METHODS } from 'http';
+// set the error message as global function
+export default {
+  computed: {
+    ...mapState(["error"]),
+// 如果sign in则只显示dashboard，如果不是则会显示 sign in  和 sign up
+    ...mapGetters({
+      auth: "isauthenticated"
+    })
+  },
+
+  methods: {
+    ...mapActions(["clearError", "logout"])
+  }
+};
+
+</script>
+
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    max-width: 1024px;
+    margin: 0 auto;
 }
+
 #nav {
-  padding: 30px;
+    padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
+    border-right: 1px solid;
+    padding: 0 10px;
+}
+
+#nav a:last-child {
+    border: none;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
 }
+
+.error {
+    background-color: rosybrown;
+    padding: 20px;
+}
+
 </style>
--- src/store.js
+++ src/store.js
@@ -1,10 +1,125 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axiosAuth from "./axios-auth";
+import router from "./router";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    idToken: null,
+    userId: null,
+    error: "",
+  },
+  mutations: {
+    AUTH_USER(state, userData) {
+      state.idToken = userData.token;
+      state.userId = userData.userId;
+    },
+    SET_ERROR(state, errorMessage) {
+      state.error = errorMessage;
+    },
+    EMPTY_ERROR(state) {
+      state.error = "";
+    },
+    CLEAR_DATA(state) {
+      state.idToken = null;
+      state.userId = null;
+    }
+  },
+  actions: {
+    signUp({commit}, authData ) {
+      axiosAuth
+        // Web API key
+        // AIzaSyCqu8s9Oz3LVK_zD6cOp2cSJhDp1WAfP24
+        // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+        .post("accounts:signUp?key=AIzaSyCqu8s9Oz3LVK_zD6cOp2cSJhDp1WAfP24", 
+        {
+          email: authData.email,
+          password: authData.password,
+          returnSecureToken: true
+        })
+        .then(res => {
+          console.log(res);
+          // Save the auth info in the state
+          commit("AUTH_USER", {
+            token: res.data.idToken,
+            userId: res.data.localId
+          });
+
+          // Local Storage
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+
+          localStorage.setItem("token",res.data.idToken)
+          localStorage.setItem("userId",res.data.localId)
+          localStorage.setItem("userId","expirationDate", expirationDate);
+
+          router.push({ name: "dashboard" });
+
+        })
+        .catch(error => {
+          console.log(error.response.data.error.message);
+          commit("SET_ERROR", error.response.data.error.message);
+        });
+      },
+
+      signIn({ commit }, authData) {
+        axiosAuth
+          .post(
+            "accounts:signInWithPassword?key=AIzaSyBlK0mKp9wuIXmtMfw9wAt22excikJZga8",
+            {
+              email: authData.email,
+              password: authData.password,
+              returnSecureToken: true
+            }
+          )
+          .then(res => {
+            console.log(res);
+            commit("AUTH_USER", {
+              token: res.data.idToken,
+              userId: res.data.localId
+            });
+
+              // Local Storage
+              const now = new Date();
+              const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+
+              localStorage.setItem("token",res.data.idToken)
+              localStorage.setItem("userId",res.data.localId)
+              localStorage.setItem("userId","expirationDate", expirationDate);
+
+              router.push({ name: "dashboard" });               
+
+
+          })
+          .catch(error => {
+            console.log(error.response.data.error.message);
+            commit("SET_ERROR", error.response.data.error.message);
+          });
+      },// closing sign in
+
+          clearError({commit}) {
+            commit ('EMPTY_ERROR');
+          },
+
+          logout ({commit}) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('expirationDate');
+            localStorage.removeItem('userId');
+
+            // commit mutation to clear the state
+            commit("CLEAR_DATA");
+            // send user to sign in routh
+            router.push({ name:"signin"});
+          }
+
+      },
+
+      getters: {
+        isauthenticated(state) {
+          return state.idToken !== null;
+      }
+  }
 });
+
